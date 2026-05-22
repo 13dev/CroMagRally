f2e5dad2b3569a4e7f0e34b35208a9a7e3edb206
Leo Oliveira <leo@13dev.pt>
2026-05-22 16:49:26 +0100

    refactor: use PowerShell for creating vcpkg binary cache directory in ReleaseBuilds workflow

--- Source/Headers/version.h
+++ Source/Headers/version.h
@@ -3,7 +3,7 @@
 #define GAME_NAME			"CroMagRally"
 #define GAME_FULL_NAME		"Cro-Mag Rally"
 #define GAME_IDENTIFIER		"io.jor.cromagrally"
-#define GAME_VERSION		"1.4.1"
+#define GAME_VERSION		"1.6.0"
 #define GAME_VERSION_MAJOR	1
-#define GAME_VERSION_MINOR	4
-#define GAME_VERSION_PATCH	1
+#define GAME_VERSION_MINOR	6
+#define GAME_VERSION_PATCH	0
